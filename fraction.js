95c5c8bf0b1a7c03b1fcdb463786fb5e7edf0c09
Musharrat-bsc <117891295+Musharrat-bsc@users.noreply.github.com>
2023-04-18 10:57:49 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,24 @@
-
+fraction class
+  const prompt=require('prompt-sync')();
+function input()
+{
+  let a=parseInt(prompt("enter the first no"));
+  let b=parseInt(prompt("enter the second no"));
+  return[a,b];
+}
+function add(a,b)
+{
+  return(a+b);
+}
+function output(a,b,c)
+{
+  console.log("%d+%d is %d",a,b,c);
+}
+function main()
+{
+  let a,b;
+  [a,b]=input();
+  c=add(a,b);
+  output(a,b,c)
+}
+main();
